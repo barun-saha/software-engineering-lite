473f1d2ba1ccc7a453449c580749582f84da7dab
Barun Saha <barun.saha04@gmail.com>
2014-08-11 23:43:20 +0530

    Removing unused SQL and JS files

--- assets/sql/create_case_study.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "isad_casestudy" (
-  "_id" int(11) NOT NULL,
-  "theory_id" int(11) NOT NULL,
-  "title" varchar(125),
-  "problem" longtext,
-  "analysis" longtext,
-  PRIMARY KEY ("_id"),
-  CONSTRAINT "theory_id_refs_id_4a79800e" FOREIGN KEY ("theory_id") REFERENCES "isad_theory" ("_id")
-);
